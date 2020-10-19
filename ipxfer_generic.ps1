c96be196068f7baee519f3909db4aeebfca718bf
wtevans <wtevans9@gmail.com>
2020-10-19 18:06:10 -0500

    Read me and help page spelling and re-write

--- ipxfer_generic.ps1
+++ ipxfer_generic.ps1
@@ -4,17 +4,26 @@ Ipxfer Script
 
 By: wtevans
 
-This script is designed to move agents to another osce server or update certificates using the ipxfer tool.
+This script is designed to run the Ipxfer to either move agents to another server or to update the current certificate if a mismatch is found.
 
 .Description
-This script is designed to run the ipxfer to either move agents to another server or to update the certificate if there is a mismatched certificate issue.   
-For this script to function properly you will need to place both of the ipxfer's (64 bit and 32 bit versions) in the same running directory as this script. The ipxfer 
-can be found on the Officescan/ApexOne on-prem server [Server Install Path]\PCCSRV\Admin\Utility\IpXfer. You will also need the ofcntcert.dat file in the same running directory
-as well found at [Server Path]\PCCSRV\Pccnt\Common\OfcNTCer.dat. There is a section in the body of the script called "Specified Variables" that need to be modified 
-for environment-specific information. For more information on ipxfer please use the links listed below. 
+For this script to function properly, you will need to place both of the ipxfer's (64 bit and 32 bit versions) along with the ofcntcert.dat file in the same running directory as this script. 
+
+•	The Ipxfer.exe 32 bit and 64 bit versions are located in the Officescan/ApexOne on-prem server [Server Install Path]\PCCSRV\Admin\Utility\IpXfer. 
+•	The ofcntcert.dat file is located in [Server Path]\PCCSRV\Pccnt\Common\OfcNTCer.dat. 
+
+You must modify this script to specify the correct values specific to your environment. Open the script and navigate to the section called "Specified Variables" you will need to specify the following values:
+
+$password = '******' replace with the unload password 
+$http_port = 80 replace with the HTTP port of osce server
+$https_port = 443 # replace with the HTTPS port of osce server
+$listening_port = 21112 # replace with the agent listening port
+$OSCE_server = "******.manage.trendmicro.com" # replace with the OSCE server name
+$updateCertificate = $true # The two allowed values are $true and $false. If $false is selected the agent will not run Ipxfer if it is reporting to the correct server already. $true is recommended in most scenarios.
+
 
 .NOTES 
-- A utf8 log should be output in the executing users %temp% directory for reference.
+- A utf8 log will be outputted in the executing users %temp% directory for reference of what occurred during the script’s execution.
 - The update certificate option can be disabled to make the script only move agents if they are reporting to the wrong server if so desired. To modify this check the "Specified Variables" section of the script.
 - If deployed with a deployment tool the ipxfer tools, ofcntcert.dat, and this script need to be in the same directory and run from that directory as well to function. 
 
